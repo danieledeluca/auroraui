8ee0e5e4c47f59b3d6a94cf869e6c9c4bd62d546
Daniele De Luca <48753350+danieledeluca@users.noreply.github.com>
2023-11-14 00:24:41 +0100

    Refactoring of JavaScript files

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -3,33 +3,36 @@
 // =====================================================
 
 // Utility classes
-const classes = {
+export const classes = {
     activeClass: 'is-active',
     navOpenClass: 'nav-open',
     parentClass: 'is-parent',
 };
 
-// Mobile breakpoint
-const mobileBreakpoint = 768;
+// Breakpoints
+export const breakpoints = {
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200,
+};
 
 /**
  * Check if is in mobile view
  *
  * @returns {Boolean}
  */
-function isMobile() {
-    return window.innerWidth < mobileBreakpoint;
+export function isMobile() {
+    return window.innerWidth < breakpoints.md;
 }
 
 /**
  * Prevent default behavior on click on empty links
  */
-function preventClickOnEmptyLinks() {
+export function preventClickOnEmptyLinks() {
     document.querySelectorAll('a[href="#"]').forEach((emptyLink) => {
         emptyLink.addEventListener('click', (e) => {
             e.preventDefault();
         });
     });
 }
-
-export { classes, mobileBreakpoint, isMobile, preventClickOnEmptyLinks };
