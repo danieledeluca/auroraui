2c9e216b270a634e210058426b6f523607f60f7a
Daniele De Luca <48753350+danieledeluca@users.noreply.github.com>
2024-01-20 16:23:04 +0100

    Docs: Update colors section

--- docs/.vitepress/theme/views/ColorsView.vue
+++ docs/.vitepress/theme/views/ColorsView.vue
@@ -4,16 +4,17 @@ import { allColors, parseColorVariable } from '../composables/colors';
 const props = defineProps<{
     color: string;
 }>();
+
+const currentColors = allColors[props.color];
 </script>
 
 <template>
     <div class="colors">
-        <div
-            v-for="[variable] in Object.entries(allColors[props.color])"
-            class="color"
-            :class="parseColorVariable(variable)"
-        >
-            <span>{{ variable }}</span>
+        <div v-for="[variable] in Object.entries(currentColors)" class="color">
+            <div class="color__preview" :style="{ backgroundColor: currentColors[variable] }"></div>
+            <div class="color__name">
+                <span>{{ parseColorVariable(variable) }}</span>
+            </div>
         </div>
     </div>
 </template>
@@ -23,39 +24,23 @@ const props = defineProps<{
 
 .colors {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
     gap: 1rem;
 
     .color {
         display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 0.25rem;
-        border-radius: 0.25rem;
+        flex-direction: column;
+        border-radius: 12px;
         font-size: $small-font-size;
         text-align: center;
-        aspect-ratio: 1;
-
-        @each $gray, $value in $grays {
-            &.#{$gray} {
-                background-color: $value;
-                color: color-contrast($value);
-            }
-        }
-
-        @each $color, $value in $colors {
-            &.#{$color} {
-                background-color: $value;
-                color: color-contrast($value);
-            }
-        }
-
-        @each $theme-color, $value in $theme-colors {
-            &.#{$theme-color} {
-                background-color: $value;
-                color: color-contrast($value);
-            }
-        }
     }
+}
+.color__preview {
+    border-radius: 12px;
+    aspect-ratio: 1;
+}
+
+.color__name {
+    margin-top: 0.5rem;
 }
 </style>
